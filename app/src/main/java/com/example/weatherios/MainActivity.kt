de6bca5a4f1c61883f87a7d4e63c9a4fc2df6788
931697478@qq.com <daxue920809>
2019-05-09 17:58:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/weatherios/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.weatherios
+
+import android.app.AlertDialog
+import android.app.PendingIntent.getActivity
+import android.content.DialogInterface
+import android.graphics.Typeface
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherios.weather.BaseDrawer
+import com.example.weatherios.weather.UiUtil
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    private var mDrawerType = BaseDrawer.Type.UNKNOWN_D
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initOnClick()
+        initWeather()
+    }
+    fun initOnClick(){
+        btn_show.setOnClickListener {
+            val builder = AlertDialog.Builder(this@MainActivity)
+            val strs = ArrayList<String>()
+            for (t in BaseDrawer.Type.values()) {
+                strs.add(t.toString())
+            }
+            var index = 0
+            for (i in 0 until BaseDrawer.Type.values().size) {
+                if (BaseDrawer.Type.values()[i] === mDrawerType) {
+                    index = i
+                    break
+                }
+            }
+            builder.setSingleChoiceItems(
+                strs.toTypedArray(), index
+            ) { dialog, which ->
+                mDrawerType = BaseDrawer.Type.values()[which]
+                notifyActivityUpdate()
+                dialog.dismiss()
+                // Toast.makeText(getActivity(), "onClick->"
+                // + which, Toast.LENGTH_SHORT).show();
+            }
+            builder.setNegativeButton(android.R.string.cancel, null)
+            builder.create().show()
+        }
+    }
+
+    private fun notifyActivityUpdate() {
+        weatherView.setDrawerType(mDrawerType)
+    }
+
+    fun  initWeather(){
+        weatherView.setDrawerType(BaseDrawer.Type.FOG_D)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        weatherView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        weatherView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        weatherView.onDestroy()
+    }
+
+}
